6c8f925f10a6f7a5c2ff7743edcf5e0f579ece9a
SeungmiNa <naissme0@gmail.com>
2022-02-07 21:37:27 -0800

    chore: install tailwindcss library

--- Frontend/src/pages/Main.js
+++ Frontend/src/pages/Main.js
@@ -2,38 +2,44 @@ import React, { useState, useEffect } from "react";
 import "../css/Main.css";
 import axios from "axios";
 import Loading from "../components/Loading";
-import { Route, Routes, Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const Main = (props) => {
   const [img, setImg] = useState(null);
   const [fileUrl, setFileUrl] = useState(null);
   const [filename, setFilename] = useState(null);
   const [taskID, setTaskID] = useState(1);
-  const [isLoading, setIsLoading] = useState(false); 
-  
-  const buttonRef = React.useRef()
+
+  const navigate = useNavigate();
+
+  const buttonRef = React.useRef();
 
   const handleUploadButtonClick = () => {
     buttonRef.current?.click();
   };
-  
+
   const uploadImg = (e) => {
+    e.preventDefault();
+    const reader = new FileReader();
     const currentFile = e.target.files[0];
-    setImg(currentFile);
+    reader.onloadend = () => {
+      setImg(currentFile);
+      setFileUrl(reader.result.toString());
+      console.log(reader.result.toString);
+    };
   };
 
   const onSubmit = () => {
-    setIsLoading(true);
     const formData = new FormData();
     formData.append("file", img);
     console.log(img);
     setFilename(img.name);
+    navigate("/result");
     axios
       .post("url ", formData)
       .then((response) => {
         console.log(response.data);
         props.onSubmit(response.data.url, response.data.result);
-        setIsLoading(false);
         alert("이미지 로딩 완료");
       })
 
@@ -43,33 +49,28 @@ const Main = (props) => {
   };
 
   return (
-    <div className="margin">
-      <div className="banner"></div>
-      <div className="subtitle"> 자신의 꽃 사진을 업로드 해주세요.</div>
-      {img == null ? <p></p> : <img src={fileUrl} />}
-      <label
-        for="profile-upload"
-        className="selectPic"
-        onClick={handleUploadButtonClick}
-        type="submit" 
-      >
-        사진 선택
-      </label>
-      <input
-        id="profile-upload"
-        type="file"
-        accept="image/*"
-        style={{ display: "none" }}
-        onChange={uploadImg}
-        ref={buttonRef}
-      />
-      {isLoading ? (
-        <Loading />
-      ) : (
-        <button onclick={onSubmit} className="result">
+    <div className="w-full">
+      <div className="banner mx-4"></div>
+      <div className="w-72 mx-auto font-bold my-12 text-base text-center">
+        자신의 꽃 사진을 업로드 해주세요.
+      </div>
+      {img == null ? <p></p> : <img src={fileUrl} alt="mainImage" />}
+      <div className="w-full flex justify-center">
+        <div className="selectPic" onClick={handleUploadButtonClick}>
+          사진 선택
+        </div>
+        <input
+          id="profile-upload"
+          type="file"
+          accept="image/*"
+          style={{ display: "none" }}
+          onChange={(e) => uploadImg(e)}
+          ref={buttonRef}
+        />
+        <button onClick={onSubmit} className="result">
           결과 보기
         </button>
-      )}
+      </div>
     </div>
   );
 };
