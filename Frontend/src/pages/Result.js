50075befe0b2de07c10f7200cf6108dc343c8b5f
SeungmiNa <naissme0@gmail.com>
2022-02-07 18:42:16 -0800

    feat: add frontend files

---
+++ Frontend/src/pages/Result.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "../css/Result.css";
+import { Link } from "react-router-dom";
+import styled from 'styled-components';
+
+
+const Result = () => {
+    return(
+        <div className='margin'>
+        <b><div className='title'>앞으로 <span className='letter'>꽃길</span>만 걷자</div></b>     
+        </div>
+    );
+}
+export default Result;
