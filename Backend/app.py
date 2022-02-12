adbc357a3095d838a0aee724335c559ac7a368fc
gilhan <rlfgks97@gmail.com>
2022-02-12 06:08:42 -0800

    build: modify response data

--- Backend/app.py
+++ Backend/app.py
@@ -99,18 +99,27 @@ def searchAPI():
 
 @app.route('/api/v1/analyze', methods=["GET"])
 def analyze():
-    id = request.args.get('id')
-    db_data = myurl.find_one(ObjectId(id))
+    db_data = myurl.find_one(
+        ObjectId(request.args.get('id'))
+    )
     # ObjectId to json
-    result = json.loads(json_util.dumps(db_data))
+    result = json.loads(
+        json_util.dumps(db_data)
+    )
     req = result["URL"]
     #############################################
     # request to AI server + 서버 분리
     #############################################
     analysis = myinform.find_one({"name": "장미"})
 
-    result = json.loads(json_util.dumps(analysis))
-    result.update({"id": result["_id"]["$oid"]})
+    result = json.loads(
+        json_util.dumps(analysis)
+    )
+    result.update(
+        {
+            "id": result["_id"]["$oid"]
+        }
+    )
     del(result['_id'])
 
     return result
@@ -144,14 +153,24 @@ def uploadFile():
     return {"id": str(result.inserted_id)}
 
 
-@app.route('/api/v1/result', methods=["GET"])
+@app.route('/api/v1/search/details', methods=["GET"])
 def respone_data():
-    id = request.form['id']
-    # print(id)
-    information = myinform.find_one({"_id": ObjectId(id)})
-    print("검색한거 타입", type(information))
-    json_information = json.loads(json_util.dumps(information))
+    json_information = json.loads(
+        json_util.dumps(
+            myinform.find_one(
+                ObjectId(
+                    request.args.get('id')
+                )
+            )
+        )
+    )
+    json_information.update(
+        {
+            "id": json_information["_id"]["$oid"]
+        }
+    )
     del(json_information['_id'])
+
     return json_information
 
 
