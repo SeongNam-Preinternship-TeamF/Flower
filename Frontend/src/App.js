812bcb23af576aa915c9e51608e34721a9b113e0
SeungmiNa <naissme0@gmail.com>
2022-02-14 23:56:28 -0800

    delete: delete search file

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -3,7 +3,6 @@ import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Main from "./pages/Main";
 import Result from "./pages/Result";
-import Search from "./pages/Search";
 import SearchResult from "./pages/SearchResult";
 import Header from "./components/Header";
 import SearchPage from "./pages/SearchPage";
